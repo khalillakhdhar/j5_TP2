50904ad7cc0794b6dc8193e5faf0224ea2cc7a01
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-26 12:09:51 +0100

    rappels

--- src/main/java/data/Rappel.java
+++ src/main/java/data/Rappel.java
@@ -1,5 +1,43 @@
 package data;
 
 public class Rappel {
+private String titre,date,description;
+
+public String getTitre() {
+	return titre;
+}
+
+public void setTitre(String titre) {
+	this.titre = titre;
+}
+
+public String getDate() {
+	return date;
+}
+
+public void setDate(String date) {
+	this.date = date;
+}
+
+public String getDescription() {
+	return description;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public Rappel(String titre, String date, String description) {
+	super();
+	this.titre = titre;
+	this.date = date;
+	this.description = description;
+}
+
+public Rappel() {
+	super();
+}
+
+
 
 }
--- src/main/java/data/Todo.java
+++ src/main/java/data/Todo.java
@@ -1,5 +1,13 @@
 package data;
 
+import java.util.ArrayList;
+
 public class Todo {
+static ArrayList<Rappel> rappels= new ArrayList<Rappel>() ;
+public void add(Rappel r)
+{
+rappels.add(r);
+}
+
 
 }
